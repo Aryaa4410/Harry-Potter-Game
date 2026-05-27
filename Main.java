c3a18cbe8b962ea7e76e07b1774e1ee3894f0ec3
Aryaa4410 <avjathan04@gmail.com>
2026-05-27 16:27:45 -0700

    Created Main.java

--- Main.java
+++ Main.java
@@ -23,5 +23,57 @@ public class Main
     scores[keyboard.nextInt()-1]++;
     System.out.println("Question 2: Pick a school subject: 1-DADA 2-Potions 3-Charms 4-Herbology");
     scores[keyboard.nextInt()-1]++;
+
+    int best = 0;
+    int bestIndex = 0;
+    for (int i = 0; i < 4; i++) 
+    {
+      if (scores[i] > best) 
+      {
+        best = scores[i];
+        bestIndex = i;
+      }
+    }
+    House myHouse = houses[bestIndex];
+    System.out.println("You are in: " + myHouse.name);
+    System.out.println("Enter your name: ");
+    keyboard.nextLine();
+    String name = keyboard.nextLine();
     
-        
+    House enemyHouse = houses[(bestIndex + 1) % 4];
+    Wizard player = new Wizard(name, 100, myHouse.spells);
+    Wizard enemy = new Wizard("Dark Wizard", 100, enemyHouse.spells);
+
+    System.out.println("DUEL START: " + player.name + " vs " + enemy.name);
+
+    while (player.isAlive() && enemy.isAlive()) 
+    {
+      System.out.println(player.name + " HP: " + player.hp + " | " + enemy.name + " HP: " + enemy.hp);
+      System.out.println("Pick spell: 1-" + player.spells[0] + " 2-" + player.spells[1] + " 3-" + player.spells[2]);
+      int choice = keyboard.nextInt();
+      int myDamage = (int)(Math.random() * 20) + 10;
+      enemy.hp = enemy.hp - myDamage;
+      System.out.println("You cast " + player.spells[choice - 1] + " for " + myDamage + " damage!");
+      if (enemy.isAlive()) 
+      {
+        int enemyDamage = (int)(Math.random() * 20) + 10;
+        player.hp = player.hp - enemyDamage;
+        System.out.println("Enemy cast " + enemy.spells[(int)(Math.random() * 3)] + " for " + enemyDamage + " damage!");
+      }
+    }
+    if (player.isAlive()) 
+    {
+      System.out.println("YOU WIN!!");
+    }
+    else 
+    {
+      System.out.println("YOU LOSE!!");
+    }
+  }
+}
+
+
+
+
+
+
